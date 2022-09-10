c803f7b1437e0e3af48bb7d8031d177e410f4e14
knagato <nagato.kasaki@gmail.com>
2022-09-11 04:13:25 +0900

    implement image api using replicate-js

---
+++ packages/web/src/@types/replicate-js.d.ts
@@ -0,0 +1,26 @@
+declare module 'replicate-js' {
+  export default class Replicate {
+    constructor(options: { token?: string, proxyUrl?: string, httpClient?: any, pollingInterval?: integer });
+    async callHttpClient({ url, method, event, body });
+    async getModel(path);
+    async getPrediction(id);
+    async startPrediction(modelVersion, input);
+    models: Model;
+  }
+  export interface ModelOptions {
+    path: string, version?: string, replicate?: Replicate
+  }
+  export class Model {
+    static async fetch(options: ModelOptions);
+    constructor(options: ModelOptions);
+    async getModelDetails();
+    async predict(input);
+    async get(path: string, version?: string)
+  }
+  export class DefaultFetchHTTPClient {
+    constructor(token);
+    async importFetch();
+    async get({ url });
+    async post({ url, body });
+  }
+};
