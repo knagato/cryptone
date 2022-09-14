0ece39f6c56039c71dad6845a33c698ae7320f56
Naoto Nishizaka <nszk.nao@gmail.com>
2022-09-15 01:20:58 +0900

    add useIsMounted

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -10,7 +10,7 @@ import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider } = configureChains(
-  [chain.polygonMumbai, chain.optimism],
+  [chain.polygonMumbai, chain.optimismGoerli],
   [
     alchemyProvider({
       apiKey: "yourAlchemyApiKey",
