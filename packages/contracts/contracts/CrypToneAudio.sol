f3d207e555d32b37f1c4d8fb481e1acc55a9dd51
toru-toruto <toruto.torutoru.toruto@gmail.com>
2022-09-21 05:58:15 +0000

    Unify tableland functions

--- packages/contracts/contracts/CrypToneAudio.sol
+++ packages/contracts/contracts/CrypToneAudio.sol
@@ -2,15 +2,14 @@
 pragma solidity 0.8.16;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "../node_modules/@tableland/evm/contracts/ITablelandTables.sol";
 import "./audio-libs/DataTypes.sol";
 import "./audio-libs/Events.sol";
 import "./audio-libs/Errors.sol";
+import "./audio-libs/TablelandManager.sol";
 import {CrypToneProfile} from "./CrypToneProfile.sol";
 
-contract CrypToneAudio is ERC1155, Ownable {
+contract CrypToneAudio is ERC1155, Ownable, TablelandManager {
     address internal profileContract;
 
     uint256 totalSupply = 0;
@@ -25,25 +24,15 @@ contract CrypToneAudio is ERC1155, Ownable {
 
     mapping(uint256 => DataTypes.RefStruct) internal refByTokenId;
 
-    // Tableland
-    string private _baseURIString =
-        "https://testnet.tableland.network/query?s=";
-    ITablelandTables private _tableland;
-    string private _metadataTable;
-    uint256 private _metadataTableId;
-    string private _tablePrefix = "canvas";
-    string private _chainName; // e.g. polygon-mumbai
-
     constructor(
         address _profileContract,
         address tableRegistry,
         string memory chainName
-    ) ERC1155("") {
+    ) ERC1155("") TablelandManager(tableRegistry, chainName) {
         if (_profileContract == address(0)) {
             revert Errors.InitParamsInvalid();
         }
         profileContract = _profileContract;
-        _initTableland(tableRegistry, chainName);
     }
 
     function setProfileNFTContract(address newContract) external onlyOwner {
@@ -64,6 +53,7 @@ contract CrypToneAudio is ERC1155, Ownable {
         return _createWork(creatorAddress, parentTokenId);
     }
 
+    // setMetadata
     function setMetadata(
         uint256 tokenId,
         uint256 generation,
@@ -75,7 +65,7 @@ contract CrypToneAudio is ERC1155, Ownable {
         if (refByTokenId[tokenId].creatorAddress != msg.sender)
             revert Errors.NotTokenOwner();
 
-        _insertNewWork(
+        super._insertNewWork(
             tokenId,
             generation,
             encryptedAudioCID,
@@ -93,7 +83,7 @@ contract CrypToneAudio is ERC1155, Ownable {
         string calldata previewAudioCID,
         string calldata jacketCID
     ) public onlyOwner {
-        _insertNewWork(
+        super._insertNewWork(
             tokenId,
             generation,
             encryptedAudioCID,
@@ -104,7 +94,6 @@ contract CrypToneAudio is ERC1155, Ownable {
     }
 
     // mint
-    // maybe, an argument 'tokenId' will be enough to refer to an audio data
     function mint(
         DataTypes.NFTType nftType,
         uint256 workId,
@@ -148,6 +137,7 @@ contract CrypToneAudio is ERC1155, Ownable {
     /// *****PRIVATE FUNCTIONS*****
     /// ****************************
 
+    // _createWork
     function _createWork(address creatorAddress, uint256 parentTokenId)
         private
         returns (uint256 newTokenId)
@@ -225,6 +215,7 @@ contract CrypToneAudio is ERC1155, Ownable {
         );
     }
 
+    // _beforeMint
     function _beforeMint(
         address creatorAddress,
         DataTypes.NFTType nftType,
@@ -253,7 +244,7 @@ contract CrypToneAudio is ERC1155, Ownable {
         _mint(creatorAddress, tokenId, amount, "");
         // setApprovalForAll( , true); // approve to market
 
-        _updateTableOnMint(tokenId, maxSupply, salesPrice);
+        super._updateTableOnMint(tokenId, maxSupply, salesPrice);
 
         emit Events.AudioMinted(nftType, creatorAddress, workId, amount);
     }
@@ -288,6 +279,7 @@ contract CrypToneAudio is ERC1155, Ownable {
         return (tokenId, maxSupply);
     }
 
+    // _beforeMintBatch
     function _beforeMintBatch(
         address creatorAddress,
         DataTypes.NFTType[] calldata types,
@@ -321,7 +313,7 @@ contract CrypToneAudio is ERC1155, Ownable {
         }
         _mintBatch(creatorAddress, ids, amounts, "");
 
-        _updateTableOnMintBatch(ids, maxSupplies, salesPrices);
+        super._updateTableOnMintBatch(ids, maxSupplies, salesPrices);
     }
 
     function _beforeMintBatchAudio(
@@ -363,176 +355,5 @@ contract CrypToneAudio is ERC1155, Ownable {
             revert Errors.ProfileAddressInvalid();
         }
         return CrypToneProfile(profileContract).profileExists(profileAddress);
-    }
-
-    /// ****************************
-    /// *****TABLELAND FUNCTIONS*****
-    /// ****************************
-
-    function _initTableland(address tableRegistry, string memory chainName)
-        private
-    {
-        _chainName = chainName;
-        _tableland = ITablelandTables(tableRegistry);
-        _metadataTableId = _tableland.createTable(
-            address(this),
-            string.concat(
-                "CREATE TABLE ",
-                _tablePrefix,
-                "_",
-                Strings.toString(block.chainid),
-                "(chain text, contractAddress text, tokenId text, ",
-                "maxSupply text, salesPrice text, totalSupply text, ",
-                "generation int, encryptedAudioCID text, ",
-                "encryptedSymmetricKey text, previewAudioCID text, jacketCID text);"
-            )
-        );
-
-        _metadataTable = string.concat(
-            _tablePrefix,
-            "_",
-            Strings.toString(block.chainid),
-            "_",
-            Strings.toString(_metadataTableId)
-        );
-    }
-
-    function _insertNewWork(
-        uint256 tokenId,
-        uint256 generation,
-        string calldata encryptedAudioCID,
-        string calldata encryptedSymmetricKey,
-        string calldata previewAudioCID,
-        string calldata jacketCID
-    ) private {
-        // too long concat makes the hardhat compile faiied.
-        string memory query = string.concat(
-            "INSERT INTO ",
-            _metadataTable,
-            // " (chain, contractAddress, tokenId, ", // polygon-mumbai
-            // "maxSupply, salesPrice, totalSupply, ",
-            // "generation, encryptedAudioCID, ",
-            // "encryptedSymmetricKey, previewAudioCID, jacketCID)",
-            " VALUES ('",
-            _chainName,
-            "','",
-            Strings.toHexString(address(this)),
-            "','"
-        );
-        query = string.concat(
-            query,
-            Strings.toString(tokenId),
-            "','0','0','0',",
-            Strings.toString(generation), // generation
-            ",'",
-            encryptedAudioCID,
-            "','"
-        );
-        query = string.concat(
-            query,
-            encryptedSymmetricKey,
-            "','",
-            previewAudioCID,
-            "','",
-            jacketCID,
-            "');"
-        );
-        _tableland.runSQL(address(this), _metadataTableId, query);
-    }
-
-    function _updateTableOnMint(
-        uint256 tokenId,
-        uint256 maxSupply,
-        uint256 salesPrice
-    ) private {
-        _tableland.runSQL(
-            address(this),
-            _metadataTableId,
-            string.concat(
-                "UPDATE ",
-                _metadataTable,
-                " SET maxSupply = '",
-                Strings.toString(maxSupply),
-                "', salesPrice = '",
-                Strings.toString(salesPrice),
-                "' WHERE tokenId = '",
-                Strings.toString(tokenId),
-                "';"
-            )
-        );
-    }
-
-    function _updateTableOnMintBatch(
-        uint256[] memory tokenIds,
-        uint256[] memory maxSupplies,
-        uint256[] memory salesPrices
-    ) private {
-        string memory suppliesPhrase = "";
-        string memory pricesPhrase = "";
-        string memory idPhrase = "";
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            suppliesPhrase = string.concat(
-                suppliesPhrase,
-                "WHEN'",
-                Strings.toString(tokenIds[i]),
-                "'THEN'",
-                Strings.toString(maxSupplies[i]),
-                "'"
-            );
-            pricesPhrase = string.concat(
-                pricesPhrase,
-                "WHEN'",
-                Strings.toString(tokenIds[i]),
-                "'THEN'",
-                Strings.toString(salesPrices[i]),
-                "'"
-            );
-            idPhrase = string.concat(
-                idPhrase,
-                "'",
-                Strings.toString(tokenIds[i]),
-                "'"
-            );
-            // Query size is limited to a maximum of 35kb.
-            uint256 strLength = bytes(suppliesPhrase).length +
-                bytes(pricesPhrase).length +
-                bytes(idPhrase).length;
-            if (strLength > 34500) {
-                // devide query
-                _runUpdateTableOnMintBatch(
-                    suppliesPhrase,
-                    pricesPhrase,
-                    idPhrase
-                );
-                suppliesPhrase = "";
-                pricesPhrase = "";
-                idPhrase = "";
-            } else if (i < tokenIds.length - 1) {
-                idPhrase = string.concat(idPhrase, ",");
-            }
-        }
-        _runUpdateTableOnMintBatch(suppliesPhrase, pricesPhrase, idPhrase);
-    }
-
-    function _runUpdateTableOnMintBatch(
-        string memory suppliesPhrase,
-        string memory pricesPhrase,
-        string memory idPhrase
-    ) private {
-        _tableland.runSQL(
-            address(this),
-            _metadataTableId,
-            string.concat(
-                "UPDATE ",
-                _metadataTable,
-                " SET maxSupply=CASE tokenId ",
-                suppliesPhrase,
-                "END,salesPrice=CASE tokenId ",
-                pricesPhrase,
-                "END WHERE tokenId IN(",
-                idPhrase,
-                ");"
-            )
-        );
     }
 }
---
+++ packages/contracts/contracts/audio-libs/TablelandManager.sol
@@ -0,0 +1,191 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import "../../node_modules/@openzeppelin/contracts/utils/Strings.sol";
+import "../../node_modules/@tableland/evm/contracts/ITablelandTables.sol";
+
+abstract contract TablelandManager {
+    // Tableland
+    string private _baseURIString =
+        "https://testnet.tableland.network/query?s=";
+    ITablelandTables private _tableland;
+    string private _metadataTable;
+    uint256 private _metadataTableId;
+    string private _tablePrefix = "canvas";
+    string private _chainName; // e.g. polygon-mumbai
+
+    constructor(address tableRegistry, string memory chainName) {
+        _initTableland(tableRegistry, chainName);
+    }
+
+    /// ****************************
+    /// *****TABLELAND FUNCTIONS*****
+    /// ****************************
+
+    function _initTableland(address tableRegistry, string memory chainName)
+        private
+    {
+        _chainName = chainName;
+        _tableland = ITablelandTables(tableRegistry);
+        _metadataTableId = _tableland.createTable(
+            address(this),
+            string.concat(
+                "CREATE TABLE ",
+                _tablePrefix,
+                "_",
+                Strings.toString(block.chainid),
+                "(chain text, contractAddress text, tokenId text, ",
+                "maxSupply text, salesPrice text, totalSupply text, ",
+                "generation int, encryptedAudioCID text, ",
+                "encryptedSymmetricKey text, previewAudioCID text, jacketCID text);"
+            )
+        );
+
+        _metadataTable = string.concat(
+            _tablePrefix,
+            "_",
+            Strings.toString(block.chainid),
+            "_",
+            Strings.toString(_metadataTableId)
+        );
+    }
+
+    function _insertNewWork(
+        uint256 tokenId,
+        uint256 generation,
+        string calldata encryptedAudioCID,
+        string calldata encryptedSymmetricKey,
+        string calldata previewAudioCID,
+        string calldata jacketCID
+    ) internal {
+        // too long concat makes the hardhat compile faiied.
+        string memory query = string.concat(
+            "INSERT INTO ",
+            _metadataTable,
+            // " (chain, contractAddress, tokenId, ", // polygon-mumbai
+            // "maxSupply, salesPrice, totalSupply, ",
+            // "generation, encryptedAudioCID, ",
+            // "encryptedSymmetricKey, previewAudioCID, jacketCID)",
+            " VALUES ('",
+            _chainName,
+            "','",
+            Strings.toHexString(address(this)),
+            "','"
+        );
+        query = string.concat(
+            query,
+            Strings.toString(tokenId),
+            "','0','0','0',",
+            Strings.toString(generation), // generation
+            ",'",
+            encryptedAudioCID,
+            "','"
+        );
+        query = string.concat(
+            query,
+            encryptedSymmetricKey,
+            "','",
+            previewAudioCID,
+            "','",
+            jacketCID,
+            "');"
+        );
+        _tableland.runSQL(address(this), _metadataTableId, query);
+    }
+
+    function _updateTableOnMint(
+        uint256 tokenId,
+        uint256 maxSupply,
+        uint256 salesPrice
+    ) internal {
+        _tableland.runSQL(
+            address(this),
+            _metadataTableId,
+            string.concat(
+                "UPDATE ",
+                _metadataTable,
+                " SET maxSupply = '",
+                Strings.toString(maxSupply),
+                "', salesPrice = '",
+                Strings.toString(salesPrice),
+                "' WHERE tokenId = '",
+                Strings.toString(tokenId),
+                "';"
+            )
+        );
+    }
+
+    function _updateTableOnMintBatch(
+        uint256[] memory tokenIds,
+        uint256[] memory maxSupplies,
+        uint256[] memory salesPrices
+    ) internal {
+        string memory suppliesPhrase = "";
+        string memory pricesPhrase = "";
+        string memory idPhrase = "";
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            suppliesPhrase = string.concat(
+                suppliesPhrase,
+                "WHEN'",
+                Strings.toString(tokenIds[i]),
+                "'THEN'",
+                Strings.toString(maxSupplies[i]),
+                "'"
+            );
+            pricesPhrase = string.concat(
+                pricesPhrase,
+                "WHEN'",
+                Strings.toString(tokenIds[i]),
+                "'THEN'",
+                Strings.toString(salesPrices[i]),
+                "'"
+            );
+            idPhrase = string.concat(
+                idPhrase,
+                "'",
+                Strings.toString(tokenIds[i]),
+                "'"
+            );
+            // Query size is limited to a maximum of 35kb.
+            uint256 strLength = bytes(suppliesPhrase).length +
+                bytes(pricesPhrase).length +
+                bytes(idPhrase).length;
+            if (strLength > 34500) {
+                // devide query
+                _runUpdateTableOnMintBatch(
+                    suppliesPhrase,
+                    pricesPhrase,
+                    idPhrase
+                );
+                suppliesPhrase = "";
+                pricesPhrase = "";
+                idPhrase = "";
+            } else if (i < tokenIds.length - 1) {
+                idPhrase = string.concat(idPhrase, ",");
+            }
+        }
+        _runUpdateTableOnMintBatch(suppliesPhrase, pricesPhrase, idPhrase);
+    }
+
+    function _runUpdateTableOnMintBatch(
+        string memory suppliesPhrase,
+        string memory pricesPhrase,
+        string memory idPhrase
+    ) internal {
+        _tableland.runSQL(
+            address(this),
+            _metadataTableId,
+            string.concat(
+                "UPDATE ",
+                _metadataTable,
+                " SET maxSupply=CASE tokenId ",
+                suppliesPhrase,
+                "END,salesPrice=CASE tokenId ",
+                pricesPhrase,
+                "END WHERE tokenId IN(",
+                idPhrase,
+                ");"
+            )
+        );
+    }
+}
